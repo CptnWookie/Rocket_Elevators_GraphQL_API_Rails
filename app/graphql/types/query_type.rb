fa06577169759d35d87c014d5e0f2629eaa5c776
“frimina” <friminazaddi@gmail.com>
2020-11-12 17:09:48 -0500

    queries modifications

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,48 @@
-
 module Types
   class QueryType < Types::BaseObject
-    field :elevators,
-          [Types::ElevatorType],
-          null: false,
-          description: "Returns a list of items in the martian library"
+    # /users
+    field :users, [Types::UserType], null: false
+
+    def users
+      User.all
+    end
+
+    # /user/:id
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
+    # user surement à enlever 
+
+    field :elevators, [Types::ElevatorType], null: false
 
     def elevators
       Elevator.all
     end
+
+    field :customers, [Types::CustomerType], null: false
+
+    def customers
+      Customer.all
+    end
+
+
+    field :addresses, [Types::AddressType], null: false
+
+    def addresses
+      Address.all
+    end
+
+
+
+
+
+
   end
 end
+
+
+
